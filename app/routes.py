ab26d6328917db9da270e088db39259e0b9b9c25
hoyt.smith <hoyt.smith@HOYT-LAPTOP>
2026-02-05 10:13:19 +0100

    Slightly more interesting test page

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,17 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, world!"
+    user = {'username': 'Hoyt'}
+    return '''
+    <html>
+        <head>
+            <title>
+                Home Page - Microblog
+            </title>
+        </head>
+        <body>
+            <h1>
+                Hello, ''' + user['username'] + '''!
+            </h1>
+        </body>
+    </html>'''
